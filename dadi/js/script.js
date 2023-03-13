01ba09bc0bf930b1bb2af267c5644d964d7f3fa5
“jdelavega94” <jcdelavega94@gmail.com>
2023-03-13 17:43:47 +0100

    project completed

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,6 +1,8 @@
 const userNumber = document.getElementById('user-number');
 const botNumber = document.getElementById('bot-number')
 const submit = document.getElementById('throw');
+const element = document.querySelector('.card-header');
+const result = document.getElementById('result')
 submit.addEventListener('click', function(){   
     let x = y = getRandomInt ;
     function getRandomInt(min, max) {
@@ -8,14 +10,31 @@ submit.addEventListener('click', function(){
         max = Math.floor(7);
         return Math.floor(Math.random() * (7 - 1) + 1);
     }
-    userNumber.innerHTML = x();
-    botNumber.innerHTML = y();
-    console.log(parseInt(userNumber.innerHTML),typeof parseInt(botNumber.innerHTML));
-    if (parseInt(userNumber.innerHTML) > parseInt(botNumber.innerHTML)) {
-        console.log('hai vinto!');
-    } else if (parseInt(userNumber.innerHTML) === parseInt(botNumber.innerHTML)) {
-        console.log('Pareggio. riprova!');
-    } else {
-        console.log('Ritenta, sarai più fortunato!');
+    const userDice = x();
+    const botDice = y();
+    let message = '';
+    let bgColor = '';
+    if (userDice > botDice) {
+        message = 'hai vinto!';
+        bgColor = 'bg-success';
+        } else if (userDice === botDice) {
+        message = 'Pareggio. riprova!';
+        bgColor = 'bg-warning';
+        } else {
+        message = 'Ritenta, sarai più fortunato!';
+        bgColor = 'bg-danger';
     }
+    console.log(userDice, botDice, message);
+    element.classList.remove('d-none')    
+    if (element.classList.contains('bg-success')){
+        element.classList.remove('bg-success');
+    } else if (element.classList.contains('bg-danger')) {
+        element.classList.remove('bg-danger'); 
+    } else if (element.classList.contains('bg-warning')) {
+        element.classList.remove('b-warning'); 
+    }
+    element.classList.add(bgColor)
+    userNumber.innerHTML = userDice;
+    botNumber.innerHTML = botDice;
+    result.innerHTML = message;
 })
