321e4d5f65b7fc7a5c182d83539b399e421a59a4
“jdelavega94” <jcdelavega94@gmail.com>
2023-03-13 16:25:55 +0100

    add mail project

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,10 +1,8 @@
 
 const userName = ['paolo.duzioni', 'olga.demina', 'roberto.parrella', 'angelo.prospero', 'alessio.libbi', 'antonio.rocco', 'stefano.martino', 'filippo.foti', 'noob.perez', 'federico.ceteroni', 'giuseppe.donnici', 'italo.cirillo', 'claudio.ciobanu', 'claudio.leone', 'cristian.natoli', 'diego.giardina', 'francesca.loria', 'francesca.perrotta', 'francesco.vinci', 'giada.genovesi', 'giorgia.capponi', 'leonardo.filesi', 'luca.pertosa', 'lorenzo.bertarelli', 'luigi.defilippo','marco.mazzotti', 'marianita.dileo', 'massimiliano.novello', 'mirko.sorrentino', 'paola.coccetti', 'pietro.savini', 'ruggero.coco', 'silvano.gul', 'silvio.salza', 'simmaco.nespoli', 'simone.viaggi', 'yared.ghebreghergis', 'john.delavega'];
-
 const submit = document.getElementById('sign-in');
 submit.addEventListener('click', function() {
 const userSearch = document.getElementById('username-input').value;
-console.log(userSearch);
 let isFound = false;
 for (let i = 0; i < userName.length; i++) {
     const currentUser = userName[i];
@@ -12,14 +10,24 @@ for (let i = 0; i < userName.length; i++) {
         isFound = true;
     }
 }
-const container = document.querySelector('.container');
-const message = document.createElement('div');
+const element = document.getElementById('output');
 let result = '';
+let bgColor = '';
 if (isFound === true){
     result = 'Accesso mail consentito';
+    bgColor = `bg-success`
 } else {
     result = 'Utente non trovato';
+    bgColor = `bg-danger`;
 }
-message.innerHTML = result;
-container.append(message)
+element.classList.remove('d-none')
+if (element.classList.contains('bg-success')){
+    element.classList.remove('bg-success');
+} else if (element.classList.contains('bg-danger')) {
+    element.classList.remove('bg-danger');    
+}
+
+element.classList.add(bgColor);
+element.innerHTML = `${result}`;
+console.log(element);
 })
